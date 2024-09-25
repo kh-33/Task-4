e3e92d1de091e06f33751081f53ccd8cb0dc37ac
kh-33 <khadeja.hegazy@gmail.com>
2024-09-25 23:20:52 +0300

    uploal1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
